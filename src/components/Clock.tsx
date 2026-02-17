25026ac61dc4be29dcda038c5e9f95e5190ee511
Totosuki <e231kari@gmail.com>
2026-02-18 02:14:19 +0900

    Clock: formatTime関数を外に出した

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,23 +1,21 @@
 import { useState, useEffect } from "react";
 
+const formatTime = () => {
+  const now = new Date();
+  const yyyy = now.getFullYear();
+  const MM = String(now.getMonth() + 1).padStart(2, '0');
+  const dd = String(now.getDate()).padStart(2, '0');
+  const hh = String(now.getHours()).padStart(2, '0');
+  const mm = String(now.getMinutes()).padStart(2, '0');
+  const ss = String(now.getSeconds()).padStart(2, '0');
+  return `${yyyy}/${MM}/${dd} ${hh}:${mm}:${ss}`;
+}
+
 function Clock() {
-  const [time, setTime] = useState('');
+  // 初回レンダリング時のみformatTime関数が実行される
+  const [time, setTime] = useState(formatTime);
 
   useEffect(() => {
-    const formatTime = () => {
-      const now = new Date();
-      const yyyy = now.getFullYear();
-      const MM = String(now.getMonth() + 1).padStart(2, '0');
-      const dd = String(now.getDate()).padStart(2, '0');
-      const hh = String(now.getHours()).padStart(2, '0');
-      const mm = String(now.getMinutes()).padStart(2, '0');
-      const ss = String(now.getSeconds()).padStart(2, '0');
-      return `${yyyy}/${MM}/${dd} ${hh}:${mm}:${ss}`;
-    }
-
-    // 初回反映用
-    setTime(formatTime());
-
     const interval = setInterval(() => {
       setTime(formatTime());
     }, 1000);
