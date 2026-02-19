3e9d4cfa8c1674bc9191319ddf1bb89c0845cb0a
Totosuki <e231kari@gmail.com>
2026-02-19 21:52:37 +0900

    Products: リンク移動ができるようにした

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,20 +4,22 @@ function Products() {
   return (
     <div className='flex flex-col divide-y divide-white my-[20px]'>
       {PRODUCTS.map((product) => (
-        <div className='flex flex-row gap-[50px] p-[30px] hover:bg-white/5 transition-colors'>
-          <div className='flex flex-col justify-between gap-[20px] w-[20%]'>
-            <p className='text-2xl'>{product.name}</p>
-            <div className='flex flex-row items-center gap-[20px]'>
-              {product.skills.map((Icon) => <Icon size={30} />)}
+        <a href={product.url} target='_blank' rel='noopener noreferrer'>
+          <div className='flex flex-row gap-[50px] p-[30px] hover:bg-white/5 transition-colors'>
+            <div className='flex flex-col justify-between gap-[20px] w-[200px]'>
+              <p className='text-2xl'>{product.name}</p>
+              <div className='flex flex-row items-center gap-[20px]'>
+                {product.skills.map((Icon) => <Icon size={30} />)}
+              </div>
+            </div>
+            <div className='flex flex-col gap-[20px]'>
+              <div>
+                {product.description.map((line) => <p>{line}</p>)}
+              </div>
+              <p>{product.period}</p>
             </div>
           </div>
-          <div className='flex flex-col gap-[20px]'>
-            <div>
-              {product.description.map((line) => <p>{line}</p>)}
-            </div>
-            <p>{product.period}</p>
-          </div>
-        </div>
+        </a>
       ))}
     </div>
   );
