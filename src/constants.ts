14035210a9a081379147de7f91f3c796e91b84c9
Totosuki <e231kari@gmail.com>
2026-02-19 18:51:15 +0900

    Products: 基本的な部分が完成

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
+import { SiDocker, SiGnubash, SiMongodb, SiPython } from "react-icons/si";
+import type { IconType } from "react-icons/lib";
+
+
 export const ASCII_TITLE = `  /$$                 /$$                                   /$$       /$$
  | $$                | $$                                  | $$      |__/
 /$$$$$$    /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$$ /$$   /$$| $$   /$$ /$$
@@ -7,3 +11,34 @@ export const ASCII_TITLE = `  /$$                 /$$                           
  |  $$$$/|  $$$$$$/  |  $$$$/|  $$$$$$/ /$$$$$$$/|  $$$$$$/| $$ \\  $$| $$
   \\___/   \\______/    \\___/   \\______/ |_______/  \\______/ |__/  \\__/|__/
 `;
+
+export type ProductType = {
+  name: string;
+  description: string[];
+  url: string;
+  period: string;
+  skills: IconType[];
+};
+
+export const PRODUCTS: ProductType[] = [
+  {
+    name: 'Observer Bot',
+    description: [
+      'Discordサーバー上のメンバーのチャット活動を記録・分析するBot。',
+      'ランキング表示やメンバーデータの閲覧など、サーバーの活性度を可視化するコマンドを提供する。',
+    ],
+    url: 'https://github.com/totosuki/discord-bot-observer',
+    period: '2023/11 ~ 2024/02',
+    skills: [SiPython, SiDocker, SiMongodb],
+  },
+  {
+    name: 'AtCoder Tester',
+    description: [
+      '競技プログラミングサイト「AtCoder」における、テスト・提出作業を自動化するCLIツール。',
+      'att test / att submit コマンド一つで、取得・実行・比較・提出まで完結できる。'
+    ],
+    url: 'https://github.com/totosuki/AtCoder-Tester',
+    period: '2023/07 ~ 2023/08',
+    skills: [SiPython, SiGnubash],
+  },
+];
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,6 +1,27 @@
+import { PRODUCTS } from "../constants";
+
 function Products() {
   return (
-    <div>test</div>
+    <div className='px-[30px] py-[20px]'>
+      <div className='flex flex-col divide-y divide-white'>
+        {PRODUCTS.map((product) => (
+          <div className='flex flex-row gap-[50px] py-[30px]'>
+            <div className='flex flex-col justify-between gap-[20px] w-[20%]'>
+              <p className='text-2xl'>{product.name}</p>
+              <div className='flex flex-row items-center gap-[20px]'>
+                {product.skills.map((Icon) => <Icon size={30} />)}
+              </div>
+            </div>
+            <div className='flex flex-col gap-[20px]'>
+              <div>
+                {product.description.map((line) => <p>{line}</p>)}
+              </div>
+              <p>{product.period}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
