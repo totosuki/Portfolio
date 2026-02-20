b8ddcfb7b0c95e2cadd2a1eb1d1278b73de2e0e5
Totosuki <e231kari@gmail.com>
2026-02-20 16:32:04 +0900

    types: Description, DescriptionSegment型を作成

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,35 @@
+import type { IconType } from "react-icons/lib";
+
 export type Tab = 'home' | 'products' | 'skills';
 
 export type TabState = {
   activeTab: Tab;
   setActiveTab: React.Dispatch<React.SetStateAction<Tab>>;
+};
+
+export type Icon = {
+  icon: IconType | string;
+  label: string;
+}
+
+export type DescriptionSegment = {
+  content: string;
+  url?: string;
+}
+
+export type Description = DescriptionSegment[];
+
+export type Product = {
+  name: string;
+  description: Description[];
+  url: string;
+  period: string;
+  skills: Icon[];
+};
+
+export type Skill = {
+  name: string;
+  description: string[];
+  star: number;
+  icons: Icon[];
 };
