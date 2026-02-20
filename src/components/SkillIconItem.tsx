e962787009c3ec6d2a55da220da768a04dfff777
Totosuki <e231kari@gmail.com>
2026-02-20 14:48:57 +0900

    SkillIconItem: react-iconsかファイルパスでアイコンを作成できるようにした

---
+++ src/components/SkillIconItem.tsx
@@ -0,0 +1,26 @@
+import type { SkillIcon } from "../constants";
+
+function SkillIconItem({ size, icon: Icon, label }: {size: number} & SkillIcon) {
+  return (
+    <div className='relative group'>
+      {typeof Icon === 'string'
+        ? <img src={Icon} alt={label} style={{ width: size, height: size }} />
+        : <Icon size={size} />
+      }
+      <span
+        className='
+          absolute -top-7 left-1/2 -translate-x-1/2
+          px-[6px] py-[2px] rounded text-xs whitespace-nowrap
+          bg-white/20 backdrop-blur-sm
+          opacity-0 group-hover:opacity-100
+          transition-opacity duration-200
+          pointer-events-none
+        '
+      >
+        {label}
+      </span>
+    </div>
+  );
+}
+
+export default SkillIconItem;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,3 +1,4 @@
+import SkillIconItem from "../components/SkillIconItem";
 import { PRODUCTS } from "../constants";
 
 function Products() {
@@ -11,23 +12,7 @@ function Products() {
               <div className='flex flex-col justify-between gap-[20px] w-[250px]'>
                 <p className='text-2xl'>{product.name}</p>
                 <div className='flex flex-row items-center gap-[20px]'>
-                  {product.skills.map((skill) => (
-                    <div className='relative group'>
-                      <skill.icon size={30} />
-                      <span
-                        className='
-                          absolute -top-7 left-1/2 -translate-x-1/2
-                          px-[6px] py-[2px] rounded text-xs whitespace-nowrap
-                          bg-white/20 backdrop-blur-sm
-                          opacity-0 group-hover:opacity-100
-                          transition-opacity duration-200
-                          pointer-events-none
-                        '
-                      >
-                        {skill.label}
-                      </span>
-                    </div>
-                  ))}
+                  {product.skills.map((skill) => <SkillIconItem size={30} icon={skill.icon} label={skill.label} /> )}
                 </div>
               </div>
               <div className='flex flex-col gap-[20px]'>
