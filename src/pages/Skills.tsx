c0c1f081bbaeda01677ca965ee47df7a2f4b9a0b
Totosuki <e231kari@gmail.com>
2026-02-23 18:43:59 +0900

    Sills: スマホのデザインを作成

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -6,25 +6,31 @@ function Skills() {
   return (
     <div className='my-[20px]'>
       <p className='text-3xl md:text-4xl md:m-[30px]'>Skills</p>
-      <div className='grid grid-cols-2 gap-[50px] md:m-[30px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-[30px] md:gap-[50px] m-[10px] mt-[30px] md:m-[30px]'>
         {SKILLS.map((skill) => (
           <div className='flex flex-col border border-white rounded-[25px] p-[30px] gap-[10px]'>
             <div className='flex flex-row justify-between items-center'>
               <div className='flex flex-col gap-[10px]'>
-                <p className='text-2xl'>{skill.name}</p>
+                {
+                  skill.name.length < 15
+                    ? <p className='text-xl md:text-2xl'>{skill.name}</p>
+                    : <p className='text-lg md:text-2xl'>{skill.name}</p>
+                }
                 <div className='flex flex-row gap-[2px] translate-x-[-3px]'>
                   {Array.from({ length: 5 }, (_, i) => {
                     return i < skill.star ? <IoMdStar size={25} /> : <IoMdStarOutline size={25} />
                   })}
                 </div>
               </div>
-              <div className='flex flex-row gap-[10px]'>
-                {skill.icons.map((icon) => <SkillIconItem size={50} icon={icon.icon} label={icon.label} />)}
+              <div className='hidden md:block'>
+                <div className='flex flex-row gap-[10px]'>
+                  {skill.icons.map((icon) => <SkillIconItem size={50} icon={icon.icon} label={icon.label} />)}
+                </div>
               </div>
             </div>
             <div>
               {skill.description.map((description) => (
-                <div>
+                <div className='break-all'>
                   {description.map((segment) => (
                     segment.url
                       ? <a
